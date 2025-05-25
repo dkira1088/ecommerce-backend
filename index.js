aacb9ce062120b00a747f6fc632f032b69f5fd2c
Christian Zuleta <darkcris89@gmail.com>
2025-05-25 16:43:59 -0500

    Se sube health status

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
 
-app.use(cors());
+
+var corsOptions = {
+  origin: 'http://webapp-123456.s3-website-us-east-1.amazonaws.com',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type'
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 dotenv.config();
 
@@ -19,6 +26,11 @@ const db = mysql.createConnection({
 db.connect(err => {
   if (err) throw err;
   console.log('MySQL conectado');
+});
+
+
+app.get('/', (req, res) => {
+  res.sendStatus(200).json({ message: 'Backend en funcionamiento' });
 });
 
 app.get('/products', (req, res) => {
